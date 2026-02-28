8cbeb3beac8c857e399a349c074510d1d38e852e
Kura <123307407+Claire969@users.noreply.github.com>
2026-02-28 13:52:58 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_BUILD_ID': JSON.stringify(process.env.npm_package_version ?? 'dev'),
+  },
   plugins: [react()],
   server: {
     host: true,
